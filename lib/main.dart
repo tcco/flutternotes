128d4a4c9513361c64e68838ad6ae95f66fcc077
Tim Co <timco92@gmail.com>
2024-07-17 16:08:51 -0700

    step-17

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutternotes/constants/routes.dart';
 import 'package:flutternotes/firebase_options.dart';
 import 'package:flutternotes/services/auth/auth_service.dart';
 import 'package:flutternotes/views/login_view.dart';
-import 'package:flutternotes/views/notes/new_note_view.dart';
+import 'package:flutternotes/views/notes/create_update_note_view.dart';
 import 'package:flutternotes/views/notes/notes_view.dart';
 import 'package:flutternotes/views/register_view.dart';
 import 'package:flutternotes/views/verify_email_view.dart';
@@ -23,7 +23,7 @@ void main() {
         registerRoute: (context) => const RegisterView(),
         notesRoute: (context) => const NotesView(),
         verifyEmailRoute: (context) => const VerifyEmailView(),
-        newNoteRoute: (context) => const NewNoteView(),
+        createOrUpdateNoteRoute: (context) => const CreateUpdateNoteview(),
       },
     ),
   );
--- lib/views/notes_list_view.dart
+++ lib/views/notes_list_view.dart
@@ -3,14 +3,19 @@ import 'package:flutter/widgets.dart';
 import 'package:flutternotes/services/crud/notes_service.dart';
 import 'package:flutternotes/utilities/dialogs/delete_dialog.dart';
 
-typedef DeleteNoteCallback = void Function(DatabaseNote note);
+typedef NoteCallback = void Function(DatabaseNote note);
 
 class NotesListView extends StatelessWidget {
-  const NotesListView(
-      {super.key, required this.notes, required this.onDeleteNote});
+  const NotesListView({
+    super.key,
+    required this.notes,
+    required this.onDeleteNote,
+    required this.onTap,
+  });
 
   final List<DatabaseNote> notes;
-  final DeleteNoteCallback onDeleteNote;
+  final NoteCallback onDeleteNote;
+  final NoteCallback onTap;
 
   @override
   Widget build(BuildContext context) {
@@ -25,6 +30,9 @@ class NotesListView extends StatelessWidget {
             softWrap: true,
             overflow: TextOverflow.ellipsis,
           ),
+          onTap: () {
+            onTap(note);
+          },
           trailing: IconButton(
             onPressed: () async {
               final shouldDelete = await showDeleteDialog(context);
